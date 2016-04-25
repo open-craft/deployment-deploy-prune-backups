ba95c19c939c187d16e687957fc65f1ac31a6c7d
Jacek Bzdak <jacek@opencraft.com>
2016-04-25 22:08:56 +0200

    Intial version

---
+++ templates/prune-job.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/tarsnap --fsck --keyfile "{{ item.keypath }}" \
+--cachedir "{{ item.cache }}" && \
+tarsnapper --config /etc/tarsnapper.yml  expire dalite-logs "{{ item.name }}"
+
